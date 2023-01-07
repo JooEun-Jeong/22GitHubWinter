d7b905112f5037000ad7934c64902a28a2816bbf
JooEun-Jeong <prichard147@naver.com>
2023-01-07 23:02:58 +0900

    가사 중반부를 마크다운파일로 프린트하는 쉘스크립트 작성

---
+++ tools/mid-generate-lyric.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+README_FILE="song.md"
+
+echo "우리 모두 다같이 고개를(wake up)" >> $README_FILE
+echo "" >> $README_FILE
+#git add $README_FILE && git commit -m "5번째 줄: 우리 모두 다같이 손뼉을"
+
+echo "우리 모두 다같이 고개를(wake up)" >> $README_FILE
+echo "" >> $README_FILE
+#git add $README_FILE && git commit -m "6번째 줄: 우리 모두 다같이 손뼉을"
+
+echo "우리 모두 다같이 즐거웁게 노래해" >> $README_FILE
+echo "![singing](https://user-images.githubusercontent.com/54920318/211147062-41fc6613-abb5-41a7-be03-31ddf6880a61.png)" >> $README_FILE
+echo "" >> $README_FILE
+#git add $README_FILE && git commit -m "7번째 줄: 우리 모두 다같이 손뼉을"
+
+echo "우리 모두 다같이 고개를(wake up)" >> $README_FILE
+echo "" >> $README_FILE
+#git add $README_FILE && git commit -m "8번째 줄: 우리 모두 다같이 손뼉을"
