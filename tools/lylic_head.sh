1957be45298f1484443493ce276f42116f3f678c
DivisonOfficer <wlssud0701@naver.com>
2023-01-07 16:18:10 +0900

    [INIT] 도입부 쉘스크립트

---
+++ tools/lylic_head.sh
@@ -0,0 +1,35 @@
+README_FILE = ".\.song.md"
+
+touch $README_FILE
+
+git add $README_FILE
+git commit -m "README 파일을 생성합니다."
+
+
+echo "우리 모두 다같이 손뼉을" >> $README_FILE
+echo "" >> $README_FILE
+echo "![poster](.\.resource/image.png)" >> $README_FILE
+echo "" >> $README_FILE
+
+
+git add $README_FILE
+git commit -m "제목 : 우리 모두 다 같이 손뼉을"
+
+echo "우리 모두 다같이 손벽을(짝짝)" >> $README_FILE
+echo "" >> $README_FILE
+git add $README_FILE
+
+git commit -m "우리 모두 다같이 손벽을(짝짝)"
+echo "우리 모두 다같이 손벽을(짝짝)" >> $README_FILE
+echo "" >> $README_FILE
+git add $README_FILE
+git commit -m "우리 모두 다같이 손벽을(짝짝)"
+echo "우리 모두 다같이 즐거웁게 노래해" >> $README_FILE
+echo "" >> $README_FILE
+git add $README_FILE
+git commit -m "우리 모두 다같이 즐거웁게 노래해"
+echo "우리 모두 다같이 손벽을(짝짝)" >> $README_FILE
+echo "" >> $README_FILE
+
+git add $README_FILE
+git commit -m "우리 모두 다같이 손벽을(짝짝)"
