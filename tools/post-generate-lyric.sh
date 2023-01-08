ef99fcf565eda8919337b1b621cb7c6957db4c59
meltingOcean <tmdgur325@gmail.com>
2023-01-08 15:34:21 +0900

    (FEAT) : 종반부 가사를 출력하느 쉘스크립트파일 생성

---
+++ tools/post-generate-lyric.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+SONG_FILE="../song.md" 
+
+echo "우리 모두 다같이 발굴러(쿵쿵)" >> $SONG_FILE
+echo "" >> $SONG_FILE
+git add $SONG_FILE
+git commit -m "종반부 1번째 줄입니다."
+echo "우리 모두 다같이 발굴러(쿵쿵)" >> $SONG_FILE
+echo "" >> $SONG_FILE
+git add $SONG_FILE
+git commit -m "종반부 2번째 줄입니다."
+echo "우리 모두 다같이 즐거웁게 노래해" >> $SONG_FILE
+echo "" >> $SONG_FILE
+git add $SONG_FILE
+git commit -m "종반부 3번째 줄입니다."
+echo "우리 모두 다같이 발굴러(쿵쿵)" >> $SONG_FILE
+echo "" >> $SONG_FILE
+git add $SONG_FILE
+git commit -m "종반부 마지막 줄입니다."
+exit 0
