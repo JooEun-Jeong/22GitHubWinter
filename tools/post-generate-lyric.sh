3fc424e5b091b3fd6aa2a401c0c28b57cbc3c60a
meltingOcean <tmdgur325@gmail.com>
2023-01-08 15:50:06 +0900

    그림을 출력하는 라인 추가

--- tools/post-generate-lyric.sh
+++ tools/post-generate-lyric.sh
@@ -17,4 +17,8 @@ echo "우리 모두 다같이 발굴러(쿵쿵)" >> $SONG_FILE
 echo "" >> $SONG_FILE
 git add $SONG_FILE
 git commit -m "종반부 마지막 줄입니다."
+echo "![koong](resource/koong.png)" >> $SONG_FILE
+echo "" >> $SONG_FILE
+git add $SONG_FILE
+git commit -m "종반부 그림."
 exit 0
